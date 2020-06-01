8b1d16fb046a371ed4cf5f1365373f9373c21301
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2020-06-01 14:06:26 -0400

    google chrome extension-master

--- main.js
+++ main.js
@@ -14,32 +14,34 @@ var y = 0;
 document.addEventListener('mousemove', function(event) {
   //Takes the mouse movement we listened for and saves it into two variables
 
-  y = event.clientY;
+   y = event.clientY + window.scrollY;
+   x = event.clientY  + (window.scrollY - 750);
+   // console.log(x)
+   // console.log(y)
+   // console.log(window.scrollY)
 
+
+  // x = event.clientX;
   mousebox();
-  console.log(y);
 
-  //Here we set the background color to the x & y value that the mouse has over the web page. See css part for rgb explaination
-  // page.style.backgroundColor = "black";
-  //By writing variable + ', ' we combine the value with text to make it write like rgb(x, y, 100); when sent to style part (css)
-  //Adds a text element to the page. It writes out the x & y value
-  // page.textContent = x + ', ' + y;
 });
+
 
 // Find the css element called 'box' to use in future
 var boxTop = document.getElementById('box-top');
+var boxBottom = document.getElementById('box-bottom');
 
 //Function for a box that follows the mouse around
 var mousebox = function() {
   //Calls the css code to push the box away from the left & top
   //the same x & y values that the mouse has
-  boxTop.style.backgroundColor = "white";
-  // box.style.opacity = "0.5";
-  // box.style.zIndex = "1";
+
   boxTop.style.width = "100vw";
-  boxTop.style.height = "34px";
-  boxTop.style.left = x + 'px';
-  boxTop.style.top = y + 'px';
+
+
+  boxTop.style.top = y + 10 + 'px';
+
+  boxBottom.style.bottom = -x  +"px";
 
 }
 
