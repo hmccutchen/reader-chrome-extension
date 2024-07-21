7ec94280a8e9346faf1a692eed9bf74b62935f4e
hmccutchen <mccutchen.hassan@gmail.com>
2024-07-20 23:09:15 -0400

    remove extension buttons to activate and deactivate extension

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
 chrome.commands.onCommand.addListener((command) => {
-  console.log("Command:", command);
   if (command === "close") {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { action: "closeShades" });
+    });
+  }
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "open") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "openShades" });
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["main.js"],
+      });
     });
   }
 });
