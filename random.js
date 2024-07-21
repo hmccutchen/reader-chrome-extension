ff476cb74e51194809f7e0f31b015e691b79f83e
hmccutchen <mccutchen.hassan@gmail.com>
2024-07-20 21:29:42 -0400

    refactor legacy code and add off shortcut

--- random.js
+++ random.js
@@ -1,4 +1,8 @@
-document.getElementsByClassName("top-shade")[0].style.display = "none";
-document.getElementsByClassName("bottom-shade")[0].style.display = "none";
-
-document.body.setAttribute("id", "false-body")
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log(message);
+  if (message.action === "closeShades") {
+    document.getElementsByClassName("top-shade")[0].style.display = "none";
+    document.getElementsByClassName("bottom-shade")[0].style.display = "none";
+    document.body.setAttribute("id", "false-body");
+  }
+});
