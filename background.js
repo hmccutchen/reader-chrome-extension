d6ee3287f7df53932075e29eec3927a1cf088ad8
hmccutchen <mccutchen.hassan@gmail.com>
2024-07-24 19:51:33 -0400

    refactor to increase and decrease DOM shaders

--- background.js
+++ background.js
@@ -1,19 +1,29 @@
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "close") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "closeShades" });
-    });
-  }
-});
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tabId = tabs[0].id;
 
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "open") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "openShades" });
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        files: ["main.js"],
-      });
-    });
-  }
+    switch (command) {
+      case "close":
+        chrome.tabs.sendMessage(tabId, { action: "closeShades" });
+        break;
+
+      case "open":
+        chrome.tabs.sendMessage(tabId, { action: "openShades" });
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          files: ["main.js"],
+        });
+        break;
+
+      case "increase":
+        chrome.tabs.sendMessage(tabId, { action: "increase" });
+        break;
+
+      case "decrease":
+        chrome.tabs.sendMessage(tabId, { action: "decrease" });
+        break;
+      default:
+        console.warn(`Unknown command: ${command}`);
+    }
+  });
 });
