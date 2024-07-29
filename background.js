3693c67f4bdacf9e3f32979220f2344d7ac4433e
hmccutchen <mccutchen.hassan@gmail.com>
2024-07-28 22:25:07 -0400

    create popup to show commands wip

--- background.js
+++ background.js
@@ -27,3 +27,19 @@ chrome.commands.onCommand.addListener((command) => {
     }
   });
 });
+
+// chrome.tabs.onActivated.addListener((activeInfo) => {
+//   chrome.scripting.executeScript({
+//     target: { tabId: activeInfo.tabId },
+//     files: ["main.js"],
+//   });
+// });
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   if (changeInfo.status === "complete") {
+//     chrome.scripting.executeScript({
+//       target: { tabId: tabId },
+//       files: ["main.js"],
+//     });
+//   }
+// });
