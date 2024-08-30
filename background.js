e17392e94a76b1b2fe7407610a59fa5bcddde448
Hassan McCutchen <mccutchen.hassan@gmail.com>
2024-08-29 23:14:24 -0400

    refactor main script (#9)

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 chrome.commands.onCommand.addListener((command) => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length === 0) {
+      console.warn("No active tab found.");
+      return;
+    }
     const tabId = tabs[0].id;
-
     switch (command) {
       case "close":
         chrome.tabs.sendMessage(tabId, { action: "closeShades" });
@@ -10,12 +13,23 @@ chrome.commands.onCommand.addListener((command) => {
 
       case "open":
         console.log("Opening shades");
-        chrome.scripting.executeScript({
-          target: { tabId: tabId },
-          files: ["main.js"],
-        });
-
-        chrome.tabs.sendMessage(tabId, { action: "openShades" });
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tabId },
+            files: ["main.js"],
+          },
+          () => {
+            if (chrome.runtime.lastError) {
+              console.error(
+                "Script injection failed:",
+                chrome.runtime.lastError
+              );
+              return;
+            }
+            // Send message only after script has been successfully injected
+            chrome.tabs.sendMessage(tabId, { action: "openShades" });
+          }
+        );
 
         break;
 
